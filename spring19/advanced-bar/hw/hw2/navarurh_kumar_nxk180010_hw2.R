4dca5e7f25c6fee1d2c5d24e9d1a568b8d77e438
Navarurh Kumar <navarurh@gmail.com>
2019-01-24 19:05:51 -0600

    changed run time calculation and reduced sims

--- spring19/advanced-bar/hw/hw2/navarurh_kumar_nxk180010_hw2.R
+++ spring19/advanced-bar/hw/hw2/navarurh_kumar_nxk180010_hw2.R
@@ -3,7 +3,7 @@ setwd('c:/data/BUAN6357/HW_2'); source('prep.txt', echo=T)
 #codeset from alookanalytics
 
 # parameters
-simulations <- 100000 # number of simulations
+simulations <- 10000 # number of simulations
 perimeter <- 1
 
 # randomly generate a point and check if it is in circle
@@ -17,21 +17,23 @@ f_point_in_circle <- function(perimeter=1){
 
 # Monte Carlo simulations
 set.seed(123)
+start_time <- Sys.time()
 pi_df <- data.frame(x=rep(NA, simulations),
                     y=rep(NA, simulations),
                     in_circle=rep(NA, simulations))
 
-system.time(
-  for (i in seq(simulations)){
+for (i in seq(simulations)){
     my_simulation <- f_point_in_circle()
     pi_df$in_circle[i] <- my_simulation$in_circle
     pi_df$x[i] <- my_simulation$x
     pi_df$y[i] <- my_simulation$y
-  }
-)
-#99.868 seconds run time
+}
 
 my_pi <- 4 * sum(pi_df$in_circle) / nrow(pi_df)
+end_time <- Sys.time()
+end_time-start_time
+
+#99.868 seconds run time
 
 #setting proper variable names for run1
 x1 <- pi_df$x
@@ -43,7 +45,9 @@ my_pi1 <- my_pi
 
 #codeset 2
 set.seed(123) #setting the same seed as prev exp
-sims <- 100000  #no of sims
+sims <- 10000  #no of sims
+start_time <- Sys.time()
+
 vec_lf <- runif(sims+sims,-1,1) #longform vector
 vec_final <- matrix(vec_lf, ncol = 2, byrow = T) #converting to 2 col matrix
 x2 <- vec_final[,1] #x coord gen
@@ -51,9 +55,12 @@ y2 <- vec_final[,2] #y coord gen
 system.time(
   in_circle2 <- (x2^2 + y2^2 <= 1)
 )
-#.001s run time!
 
 my_pi2 <- 4 * sum(in_circle2) / length(in_circle2)
+end_time <- Sys.time()
+end_time-start_time
+
+#.001s run time!
 
 #checks
 all.equal(x1, x2)
