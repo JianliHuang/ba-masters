4f952a6b72e49f27cf0a7d2468d5764aa2ae2651
navarurh <navarurh@gmail.com>
2019-02-06 15:56:40 -0600

    aml: lab2 edits

--- spring19/advanced-bar/hw/hw3/nxk180010_hw3.R
+++ spring19/advanced-bar/hw/hw3/nxk180010_hw3.R
@@ -1,13 +1,33 @@
 library(MASS)
-library(tidyverse)
-library(broom)
+#library(tidyverse)
+#library(broom)
 
+#df declaration and hc dendro creation
 df <- Boston
 hc <- hclust(dist(df))
 
+#defining cutree limits
 x <- c(1:25)
-hc.twss <- cutree(hc,k = x)
+#creating cutree's for all cluster counts
+hc.clusts <- cutree(hc,k = x)
 
+#function to eval the centroids based on cutree
+clust.centroid <- function(i, dat, clusters) {
+  ind <- (clusters == i)
+  colMeans(dat[ind,])
+}
+
+#centroid list empty init
+centroid.list <- list()
+
+#looping to get cluster centres for different cutree cluster counts
+for(i in 1:25)
+{
+  centroid.list[[i]] <- sapply(unique(hc.clusts[,i]), clust.centroid, df, hc.clusts[,i])
+}
+
+
+#finding the twss for diff cluster counts from kmeans
 km <- c()
 kmtwss <- c()
 for (i in 1:25)
@@ -16,7 +36,3 @@ for (i in 1:25)
   km <- cbind(km,temp$cluster)
   kmtwss <- rbind(kmtwss,temp$tot.withinss)
 }
-
-
-z <- hc.twss
-z
---
+++ spring19/applied-ml/lab2/lab2.py
@@ -0,0 +1,3 @@
+import pandas as pd
+ad = pd.read_csv('adult.csv')
+au = pd.read_csv('auto.csv')
