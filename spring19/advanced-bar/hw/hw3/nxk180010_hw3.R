1ee4f80cec8dacc6f7164ef555ad6047b10b3073
navarurh <navarurh@gmail.com>
2019-02-06 18:04:01 -0600

    abar: hw3 sol 1

--- spring19/advanced-bar/hw/hw3/nxk180010_hw3.R
+++ spring19/advanced-bar/hw/hw3/nxk180010_hw3.R
@@ -1,6 +1,7 @@
+setwd('c:/data/BUAN6357/HW_3'); source('prep.txt', echo=T)
+
+#libs used
 library(MASS)
-#library(tidyverse)
-#library(broom)
 
 #df declaration and hc dendro creation
 df <- Boston
@@ -8,31 +9,35 @@ hc <- hclust(dist(df))
 
 #defining cutree limits
 x <- c(1:25)
+
 #creating cutree's for all cluster counts
-hc.clusts <- cutree(hc,k = x)
+hc.clusts <- as.data.frame(cutree(hc,k = x))
 
-#function to eval the centroids based on cutree
-clust.centroid <- function(i, dat, clusters) {
-  ind <- (clusters == i)
-  colMeans(dat[ind,])
-}
+#temping df
+dft <- df
+#temp holder for wss values
+temp <- matrix(0, nrow = 25, ncol = 25)
 
-#centroid list empty init
-centroid.list <- list()
-
-#looping to get cluster centres for different cutree cluster counts
-for(i in 1:25)
-{
-  centroid.list[[i]] <- sapply(unique(hc.clusts[,i]), clust.centroid, df, hc.clusts[,i])
-}
-
-
-#finding the twss for diff cluster counts from kmeans
-km <- c()
-kmtwss <- c()
+#looping to evaluate individual wss values
 for (i in 1:25)
 {
-  temp <- kmeans(df,i)
-  km <- cbind(km,temp$cluster)
-  kmtwss <- rbind(kmtwss,temp$tot.withinss)
+  dft$clust <- hc.clusts[,i]
+  for (j in 1:i)
+  {
+    dfts <- dft[which(dft$clust == j),]
+    a <- as.matrix(dist(dfts))
+    b <- a^2
+    temp[i,j] <- sum(b)/2
+  }
 }
+
+#temp holder for total wss
+hc.twss <- rep(-1,25)
+
+#simple summing to get twss values
+for(i in 1:25)
+{
+  hc.twss[i] <- sum(temp[i,])
+}
+
+source('validate.txt', echo=T)
